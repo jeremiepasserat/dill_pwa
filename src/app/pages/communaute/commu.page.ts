dc2d4aa0694462bb86a59ce12eec4d037d1f6c86
mouad <mouad.chaib@@etu.univ-orleans.fr>
2021-02-27 21:04:45 +0100

    integration openstreetmap

--- src/app/pages/communaute/commu.page.ts
+++ src/app/pages/communaute/commu.page.ts
@@ -3,6 +3,10 @@ import {MenuController, ModalController} from '@ionic/angular';
 import {AuthService} from 'src/app/services/auth.service';
 import {Utilisateur} from 'src/app/models/utilisateur';
 import {ProfilPage} from '../profil/profil.page';
+import { Platform } from '@ionic/angular';
+import { Router } from '@angular/router';
+import { Map, tileLayer, marker, icon, L } from 'leaflet';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-menu',
@@ -11,13 +15,15 @@ import {ProfilPage} from '../profil/profil.page';
 })
 
 export class CommuPage implements OnInit {
-
+  map: L.Map;
   utilisateur: Utilisateur;
 
   constructor(
     private menu: MenuController,
     private authService: AuthService,
-    private modalController: ModalController
+    private modalController: ModalController,
+    private plt: Platform,
+    private router: Router
   ) {
     this.menu.enable(false);
   }
@@ -26,10 +32,29 @@ export class CommuPage implements OnInit {
 
   }
 
+  ngAfterViewInit() {
+    this.plt.ready().then(() => {
+      this.initMap();
+    });
+  }
+
   async profil() {
     const profilModal = await this.modalController.create({
       component: ProfilPage,
     });
     return await profilModal.present();
+  }
+
+  initMap(){
+     this.map = new Map('map').setView([33.6396965, -84.4304574], 8);
+
+    tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+          attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        }).addTo(this.map);
+
+    setTimeout(()=>{
+      this.map.invalidateSize();
+    });
+
   }
 }
